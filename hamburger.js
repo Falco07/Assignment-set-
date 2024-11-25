41792f655be04111a95d97b2d5471b021d3e48d6
falco <henryezeaneche@gmail.com>
2024-11-25 16:02:45 +0100

    partially fixed hamburger

---
+++ hamburger.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector('.hamburger')
+const navMenu = document.querySelector('.navList')
+
+hamburger.addEventListener('click',()=>{
+    navMenu.classList.toggle('navList--open') ;
+})
+
+
+
+hamburger.addEventListener('click',)
+
+hamburger.classList.remove('active')
+navMenu.classList.remove('acitve')
--- sixth.js
+++ sixth.js
@@ -1,20 +1,11 @@
-// const hamburger = document.querySelector('.hamburger')
-// const navMenu = document.querySelector('.list')
-// hamburger.addEventListener('click',()=>{
-//     hamburger.classList.toggle('active')
-//     navMenu.classList.toggle('active')
-// })
-// document.querySelectorAll('.list').forEach(n=>n.addEventListener('click',()=>
-// hamburger.classList.remove('active');
-// navMenu.classList.remove('active');
-// ))
+
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.list');
 
 // Toggle the active class on the hamburger and navMenu when clicked
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
-    navMenu.classList.toggle('active');
+    navMenu.classList.toggle('navigation--open');
     console.log('button clicked')
 });
 
