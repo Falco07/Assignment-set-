1006cf4682caedd8381b41334bfaf9d03bda24b1
falco <henryezeaneche@gmail.com>
2024-11-26 12:54:16 +0100

    added hamburger

---
+++ fifth.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector('.hamburger')
+const navMenu = document.querySelector('.navLinks')
+
+hamburger.addEventListener('click',()=>{
+    navMenu.classList.toggle('navLinks--open') ;
+})
+
+
+
+hamburger.addEventListener('click',)
+
+hamburger.classList.remove('active')
+navMenu.classList.remove('acitve')
